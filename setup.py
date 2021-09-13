4c92672f1f6933f6457c63f96d94bd32a7abb06d
Jose Mielgo <mielgosez@gmail.com>
2021-09-13 14:49:19 +0200

    Fixing import bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="simio_lisa",
-    version="0.0.3",
+    version="0.1.0",
     author="Jose Mielgo",
     author_email="mielgosez@gmail.com",
     description="Beautiful Simio",
