70a2c7c1549e17bdb1acf18539455d0591a337af
Jose Mielgo <mielgosez@gmail.com>
2021-09-16 10:12:08 +0200

    Refactoring names and updating requirements

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     install_requires=["pandas",
-                      "numpy"]
+                      "numpy",
+                      "plotly"]
 )
